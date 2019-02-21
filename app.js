e5e6d6ef97252667daa9fd02867a77093018a7f1
Patrick Ford <patrickford@msn.com>
2019-02-20 19:44:06 -0800

    Question data and display

--- app.js
+++ app.js
@@ -2,4 +2,43 @@ $(function() {
 
   console.log("code running");
 
+  let quiz = {
+    name: "Food",
+    questions: [
+      {
+        text: "What ingredient does not go in Mac and Cheese?",
+        answers: ["Macaroni", "Pickles", "Cheese"],
+        correct: 1
+      },
+      {
+        text: "Which of these is a fruit?",
+        answers: ["Eggplant", "Pickles", "Cherry"],
+        correct: 2        
+      },
+      {
+        text: "Which of these is a berry?",
+        answers: ["Banana", "Peach", "Raspberry"],
+        correct: 2        
+      }
+    ]
+  }
+
+  let count = 0;
+  let score = 0;
+
+  function showQuestion(count) {
+    $("#question").empty();
+    let question = `<h3> ${quiz.questions[count].text}`;
+    $("#question").append(question);
+
+    let answers = "<form>";
+    for (let i=0; i<quiz.questions[count].answers.length; i++) {
+      answers += `<input type="radio" name="answer" value="${i}"> ${quiz.questions[count].answers[i]}<br>`
+    }
+    answers += "<input type='submit'></form>";
+    $("#question").append(answers);
+  }
+
+  showQuestion(count);
+
 })
